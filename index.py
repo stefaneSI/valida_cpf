45eb0fa2039db5e08c8ac650287c4058280c21e1
Stefane Brito <stefane_brito@hotmail.com>
2020-11-05 02:34:55 -0300

    forma_valida_cpf

---
+++ index.py
@@ -0,0 +1,9 @@
+#multipliaca os 9 primeiros digitos pelos numeros de 10 a 2
+#soma o resultado, multiplicapor 10 e divide por 11
+#verifica o resto da divisão - deve ser igual ao primeiro digito verificador
+
+#multipliaca os 9 primeiros digitos e o primeiro digito verificado pelos numeros de 11 a 2
+#soma o resultado, multiplicapor 10 e divide por 11
+#verifica o resto da divisão - deve ser igual ao segundo digito verificador
+
+#CPFs com todos os digitos iguais entram na regra de cima mas são invalidos
