e2dd45781cc4b3097cefad552ce276ac8b8ece95
Stefane Brito <stefane_brito@hotmail.com>
2020-11-09 21:53:45 -0300

    Alteracoes finais no código

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, request, render_template
+import valida
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/health')
 def index():
     return render_template('index.html')
+
+if valida.verifica_digitos('654654654528') == False:
+    print('CPF invalido')
+else:
+    if valida.valida_cpf('654654654528') == False:
+        print('CPF INVALIDO')
+    else:
+        print('CPF VALIDO')
+    
+
 
 #roda a aplicação localhost na porta 9095
 if __name__ == '__main__':
